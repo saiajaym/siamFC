b3244eca2990701e38b27ddb673f72a9fafe240e
saiajaym <saiajaym@gmail.com>
2020-02-29 13:52:53 -0500

    code correction

--- siamfc/train.py
+++ siamfc/train.py
@@ -7,7 +7,7 @@ from siamfc import TrackerSiamFC
 
 
 if __name__ == '__main__':
-    root_dir = os.path.expanduser('~/data/GOT-10k')
+    root_dir = os.path.expanduser('~/data/got-10k')
     seqs = GOT10k(root_dir, subset='train', return_meta=True)
 
     tracker = TrackerSiamFC()
